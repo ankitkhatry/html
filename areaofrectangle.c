d377810acc22111f94fb48c0b9315de274d6551a
Ankit Khatry <154233487+ankitkhatry@users.noreply.github.com>
2024-03-20 08:44:08 +0545

    Create areaofrectangle.c

---
+++ areaofrectangle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+int l,b,a;
+printf("Enter the length and breadth:");
+scanf("%d%d", &l,&b);
+a=l*b;
+printf("Area is %d", a);
+return 0;
+  // Written by Ankit Khatry
+}
